7a4f72eee6af89c8e6c1c71548e5646fded41d7a
corydave <corydave@users.noreply.github.com>
2025-01-28 18:54:27 -0500

    Create hello_world.py

---
+++ 01/hello_world.py
@@ -0,0 +1,54 @@
+# FILE NAME: hello_world.py
+
+# NAME: 
+# DATE: 
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+
+
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+    
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+Hello, World!
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+1. Congratulations on your first lab! What is ONE thing you learned?
+
+
+
+
+
+'''
